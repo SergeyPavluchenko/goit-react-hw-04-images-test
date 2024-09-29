35921b134ebe9035e83e9d09ffc573708779a6f8
Sergey <s.pavluchenko90@gmail.com>
2024-09-29 14:36:15 +0300

    useState

--- src/components/ImageItem/ImageItem.js
+++ src/components/ImageItem/ImageItem.js
@@ -1,35 +1,28 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { ImgPic } from './ImageItemStyled';
 import { Modal } from '../Modal/Modal';
 
-export class ImageItem extends Component {
-  state = {
-    showModal: false,
-  };
+export const ImageItem = props => {
+  const [showModal, setShowModal] = useState(false);
 
-  toggleModal = () => {
-    this.setState(state => ({
-      showModal: !state.showModal,
-    }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
+  const { id, webformatURL, tags, largeImageURL } = props.image;
 
-  render() {
-    const { showModal } = this.state;
-    const { webformatURL, tags, largeImageURL } = this.props.image;
-    return (
-      <>
-        <div>
-          <ImgPic onClick={this.toggleModal} src={webformatURL} alt={tags} />
-          <p>{tags}</p>
-        </div>
-        {showModal && (
-          <Modal
-            onClose={this.toggleModal}
-            tags={tags}
-            largeImageURL={largeImageURL}
-          />
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <div key={id} onClick={toggleModal}>
+        <ImgPic src={webformatURL} alt={tags} />
+        <p>{tags}</p>
+      </div>
+      {showModal && (
+        <Modal
+          onClose={toggleModal}
+          tags={tags}
+          largeImageURL={largeImageURL}
+        />
+      )}
+    </>
+  );
+};
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,42 +1,61 @@
-import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 import { ImgStyle, ModalWindow, Overlay } from './ModalStyled';
 
 const ModaRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  // componentDidMount = () => {
-  //   window.addEventListener('keydown', this.hendleClick);
-  // };
+export function Modal(props) {
 
-  // componentWillUnmount = () => {
-  //   window.removeEventListener('keydown', this.hendleClick);
-  // };
-
-  closeMOdal = () => {
-    this.props.onClose();
+  const closeMOdal = () => {
+    props.onClose();
   };
 
-  hendleClick = e => {
+  const hendleClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      props.onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.hendleClick}>
-        <ModalWindow>
-          <ImgStyle src={this.props.largeImageURL} alt={this.props.tags} />
-          <AiOutlineCloseCircle
-            color="white"
-            cursor="pointer"
-            onClick={this.closeMOdal}
-          />
-        </ModalWindow>
-      </Overlay>,
-      ModaRoot
-    );
-  }
+  return createPortal(
+    <Overlay onClick={hendleClick}>
+      <ModalWindow>
+        <ImgStyle src={props.largeImageURL} alt={props.tags} />
+        <AiOutlineCloseCircle
+          color="white"
+          cursor="pointer"
+          onClick={closeMOdal}
+        />
+      </ModalWindow>
+    </Overlay>,
+    ModaRoot
+  );
 }
+
+// export class Modal extends Component {
+
+//   closeMOdal = () => {
+//     this.props.onClose();
+//   };
+
+//   hendleClick = e => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <Overlay onClick={this.hendleClick}>
+//         <ModalWindow>
+//           <ImgStyle src={this.props.largeImageURL} alt={this.props.tags} />
+//           <AiOutlineCloseCircle
+//             color="white"
+//             cursor="pointer"
+//             onClick={this.closeMOdal}
+//           />
+//         </ModalWindow>
+//       </Overlay>,
+//       ModaRoot
+//     );
+//   }
+// }
